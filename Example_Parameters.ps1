6aec96867fc014a7ce403fc8d34adfa7b13c1040
MatW <lmw94002@yahoo.com>
2024-09-05 21:48:21 -0400

    Update Example_Parameters.ps1

--- Example_Parameters.ps1
+++ Example_Parameters.ps1
@@ -9,7 +9,7 @@
         Validating inputs will help prevent unexpected outcomes in your script
         https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-5.1
     .example
-        PS> .\Example_Parameters.ps1
+        PS> .\Example_Parameters.ps1 -myVar "Hello World"
 #>
 
 param (
