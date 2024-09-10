3a3579fccc9233ccead057b9cae41e6dc2e62079
MatW <lmw94002@yahoo.com>
2024-09-09 22:31:59 -0400

    2 more examples to create

---
+++ Example_Splatting.ps1
@@ -0,0 +1,15 @@
+<#
+    .synopsis
+        Example of using Splatting
+    .description
+        Example of Splatting in PowerShell
+    .link
+        https://github.com/lmw94002/powershell-examples
+    .notes
+        This should be the starter template for all your scripts. 
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_splatting?view=powershell-5.1
+    .example
+        PS> .\Example_Splatting.ps1
+#>
+
+write-output "Hello World"
---
+++ Example_WildcardsRegEx.ps1
@@ -0,0 +1,16 @@
+<#
+    .synopsis
+        Example of using Wildcard and RegEx
+    .description
+        Example of using Wildscards and Regular Expressions in PowerShell
+    .link
+        https://github.com/lmw94002/powershell-examples
+    .notes
+        This should be the starter template for all your scripts. 
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_wildcards?view=powershell-5.1
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_regular_expressions?view=powershell-5.1
+    .example
+        PS> .\Example_WildcardsRegEx.ps1
+#>
+
+write-output "Hello World"
