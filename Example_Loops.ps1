91bf3955034ebeddfd42c5a55b8f961416d7adf3
Mathew Walker <Mathew.Walker@ll.mit.edu>
2024-09-11 08:52:52 -0400

    More Scripts and Tweaks

--- Example_Loops.ps1
+++ Example_Loops.ps1
@@ -1,13 +1,16 @@
 <#
     .synopsis
-        Example of Comment Based Help
+        Examples of Loops using FOR, FOREACH, DO/WHILE, WHILE
     .description
-        Example of Comment Based Help for PowerShell Scripting
+        Examples of FOR, FOREACH, DO/WHILE, WHILE for PowerShell Scripting
     .link
         https://github.com/lmw94002/powershell-examples
     .notes
-        This should be the starter template for all your scripts. 
-        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1 
+        Loops using FOR, FOREACH, DO/WHILE, WHILE
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_for?view=powershell-5.1
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_foreach?view=powershell-5.1
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_do?view=powershell-5.1
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_while?view=powershell-5.1
     .example
         PS> .\Example_Loops.ps1
 #>
--- Example_OutputRedirection.ps1
+++ Example_OutputRedirection.ps1
@@ -6,9 +6,10 @@
     .link
         https://github.com/lmw94002/powershell-examples
     .notes
-        This should be the starter template for all your scripts. 
+        Examples of different output streams, redirecting streams and writing to files. 
         https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_output_streams?view=powershell-5.1
         https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_redirection?view=powershell-5.1
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/tee-object?view=powershell-5.1
     .example
         PS> .\Example_OutputRedirection.ps1
 #>
