1c18118dcb137912b26dc8af2ef18d637985db08
Mathew Walker <Mathew.Walker@ll.mit.edu>
2024-09-12 16:51:44 -0400

    New Script, Info Update

---
+++ Example_Switch.ps1
@@ -0,0 +1,15 @@
+<#
+    .synopsis
+        Example of using "Switch"
+    .description
+        Example of using Switch statements in your powershell code
+    .link
+        https://github.com/lmw94002/powershell-examples
+    .notes
+        This should be the starter template for all your scripts. 
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_switch?view=powershell-5.1
+    .example
+        PS> .\Example_Switch.ps1 
+#>
+
+write-output "Hello World"
