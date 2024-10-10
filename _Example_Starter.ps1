88d5db975800498092a711a4df9319d71890b93f
Mathew Walker <Mathew.Walker@ll.mit.edu>
2024-10-10 09:31:54 -0400

    splitpath and measureobject

---
+++ _Example_Starter.ps1
@@ -0,0 +1,15 @@
+<#
+    .synopsis
+        Copy this Script for Starting New Example
+    .description
+        Start here when making a new PowerShell Script Example
+    .link
+        https://github.com/lmw94002/powershell-examples
+    .notes
+        Loops using FOR, FOREACH, DO/WHILE, WHILE
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_for?view=powershell-5.1
+    .example
+        PS> .\Example_Starter.ps1
+#>
+
+write-output "Hello World"
