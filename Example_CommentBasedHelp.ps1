16967597c591bec28de0a94c58f8674795be6d18
MatW <lmw94002@yahoo.com>
2025-01-18 20:01:49 -0500

    minor formatting update

--- Example_CommentBasedHelp.ps1
+++ Example_CommentBasedHelp.ps1
@@ -1,19 +1,25 @@
 <#
-    .synopsis
-        Example of Comment Based Help
-    .description
-        Example of Comment Based Help for PowerShell Scripting
-    .parameter foo
-        Sets the FOO variable to the value provided
-    .link
-        https://github.com/lmw94002/powershell-examples
-    .notes
-        This should be the starter template for all your scripts. 
-        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1 
-    .example
-        PS> get-help .\Example_CommentBasedHelp.ps1 -Full
-    .example
-        PS> .\Example_CommentBasedHelp.ps1 
+.synopsis
+Example of Comment Based Help
+
+.description
+Example of Comment Based Help for PowerShell Scripting
+
+.parameter foo
+Sets the FOO variable to the value provided
+
+.link
+https://github.com/lmw94002/powershell-examples
+
+.notes
+This should be the starter template for all your scripts. 
+https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1 
+
+.example
+PS> get-help .\Example_CommentBasedHelp.ps1 -Full
+
+.example
+PS> .\Example_CommentBasedHelp.ps1 
 #>
 
 param (
